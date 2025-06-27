c565ce133ca7464c374ed78998119e5587baff5f
debora <86936640+deboralawall@users.noreply.github.com>
2025-06-26 21:42:11 -0300

    Add files via upload

---
+++ app/app/src/main/java/com/example/appudescar/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.appudescar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Ação de clicar no botão 'pegar carona' vai para a tela de agendar uma carona
+        Button btn_pesquisar_carona = findViewById(R.id.btn_pesquisar);
+
+        btn_pesquisar_carona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, PegarCarona.class);
+                startActivity(intent);
+            }
+        });
+        //--------------------------------------------------------------------------
+
+        //Ação de clicar no botão 'oferecer carona' vai para a tela de oferta de carona
+        Button btn_oferecer_carona = findViewById(R.id.btn_oferecer);
+
+        btn_oferecer_carona.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, OferecerCarona.class);
+                startActivity(intent);
+            }
+        });
+        //--------------------------------------------------------------------------
+
+    }
+    
+}
---
+++ app/app/src/main/java/com/example/appudescar/OferecerCarona.java
@@ -0,0 +1,39 @@
+package com.example.appudescar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class OferecerCarona extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_oferecer_carona);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //Ação de clicar no botão 'voltar' vai para home
+        Button btn_voltar = findViewById(R.id.btn_voltar);
+
+        btn_voltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OferecerCarona.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        //--------------------------------------------------------------------------
+    }
+}
